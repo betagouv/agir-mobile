0de2fdf562abfdde2371e5260e23fdfa24819734
Lucas SAUDON <lucas.saudon@beta.gouv.fr>
2024-05-22 08:41:38 +0200

    chore: Add lints

--- agir/test/main_test.dart
+++ agir/test/main_test.dart
@@ -1,10 +1,10 @@
 import 'package:agir/main.dart';
 import 'package:flutter_test/flutter_test.dart';
 
-main() {
-  testWidgets('show Hello World', (tester) async {
+void main() {
+  testWidgets('show Hello World', (final WidgetTester tester) async {
     await tester.pumpWidget(const MainApp());
     await tester.pumpAndSettle();
-    expect(find.text("Hello World!"), findsOneWidget);
+    expect(find.text('Hello World!'), findsOneWidget);
   });
 }
