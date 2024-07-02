9f8ad0d7300a872b1d495be9ca2f920172826994
Lucas SAUDON <lucas.saudon@beta.gouv.fr>
2024-07-02 17:42:08 +0200

    fix: Supprimer mon compte

--- app/lib/features/profil/supprimer_compte/presentation/widgets/supprimer_compte.dart
+++ app/lib/features/profil/supprimer_compte/presentation/widgets/supprimer_compte.dart
@@ -1,7 +1,5 @@
 // ignore_for_file: move-variable-closer-to-its-usage
 
-import 'package:app/features/authentification/domain/ports/authentification_port.dart';
-import 'package:app/features/profil/domain/ports/profil_port.dart';
 import 'package:app/features/profil/presentation/widgets/profil_titre_partie.dart';
 import 'package:app/features/profil/supprimer_compte/presentation/blocs/supprimer_compte_bloc.dart';
 import 'package:app/features/profil/supprimer_compte/presentation/blocs/supprimer_compte_event.dart';
@@ -15,18 +13,13 @@ class SupprimerCompte extends StatelessWidget {
   const SupprimerCompte({super.key});
 
   Future<void> _handleSupprimerCompte(final BuildContext context) async {
-    final authentificationPort = context.read<AuthentificationPort>();
-    final profilPort = context.read<ProfilPort>();
+    final bloc = context.read<SupprimerCompteBloc>();
     final result = await DsfrModal.showModal<bool>(
       context: context,
       builder: (final context) => const SupprimerCompteModal(),
     );
     if (result != null && result) {
-      final bloc = SupprimerCompteBloc(
-        authentificationPort: authentificationPort,
-        profilPort: profilPort,
-      )..add(const SupprimerCompteSuppressionDemandee());
-      await bloc.close();
+      bloc.add(const SupprimerCompteSuppressionDemandee());
     }
   }
 
@@ -34,49 +27,59 @@ class SupprimerCompte extends StatelessWidget {
   Widget build(final BuildContext context) {
     const color = DsfrColors.error425;
 
-    return Column(
-      crossAxisAlignment: CrossAxisAlignment.stretch,
-      children: [
-        const ProfilTitrePartie(titre: Localisation.supprimerVotreCompte),
-        const SizedBox(height: DsfrSpacings.s2w),
-        const Row(
-          crossAxisAlignment: CrossAxisAlignment.start,
+    return BlocProvider(
+      create: (final context) => SupprimerCompteBloc(
+        authentificationPort: context.read(),
+        profilPort: context.read(),
+      ),
+      child: Builder(
+        builder: (final context) => Column(
+          crossAxisAlignment: CrossAxisAlignment.stretch,
           children: [
-            Padding(
-              padding: EdgeInsets.only(top: 3),
-              child: Icon(
-                DsfrIcons.systemFrInfoFill,
-                size: 16,
-                color: color,
-              ),
-            ),
-            SizedBox(width: DsfrSpacings.s1v),
-            Expanded(
-              child: Text.rich(
-                TextSpan(
-                  children: [
-                    TextSpan(text: Localisation.supprimerVotreCompteContenu),
-                    TextSpan(
-                      text: Localisation
-                          .attentionAucuneDonneeNePourraEtreRecuperee,
-                      style: DsfrTextStyle.bodyXsBold(color: color),
-                    ),
-                  ],
+            const ProfilTitrePartie(titre: Localisation.supprimerVotreCompte),
+            const SizedBox(height: DsfrSpacings.s2w),
+            const Row(
+              crossAxisAlignment: CrossAxisAlignment.start,
+              children: [
+                Padding(
+                  padding: EdgeInsets.only(top: 3),
+                  child: Icon(
+                    DsfrIcons.systemFrInfoFill,
+                    size: 16,
+                    color: color,
+                  ),
                 ),
-                style: DsfrTextStyle.bodyXs(color: color),
-              ),
+                SizedBox(width: DsfrSpacings.s1v),
+                Expanded(
+                  child: Text.rich(
+                    TextSpan(
+                      children: [
+                        TextSpan(
+                          text: Localisation.supprimerVotreCompteContenu,
+                        ),
+                        TextSpan(
+                          text: Localisation
+                              .attentionAucuneDonneeNePourraEtreRecuperee,
+                          style: DsfrTextStyle.bodyXsBold(color: color),
+                        ),
+                      ],
+                    ),
+                    style: DsfrTextStyle.bodyXs(color: color),
+                  ),
+                ),
+              ],
+            ),
+            const SizedBox(height: DsfrSpacings.s2w),
+            DsfrButton(
+              label: Localisation.supprimerVotreCompte,
+              icon: DsfrIcons.systemFrWarningLine,
+              variant: DsfrButtonVariant.secondary,
+              size: DsfrButtonSize.lg,
+              onTap: () async => _handleSupprimerCompte(context),
             ),
           ],
         ),
-        const SizedBox(height: DsfrSpacings.s2w),
-        DsfrButton(
-          label: Localisation.supprimerVotreCompte,
-          icon: DsfrIcons.systemFrWarningLine,
-          variant: DsfrButtonVariant.secondary,
-          size: DsfrButtonSize.lg,
-          onTap: () async => _handleSupprimerCompte(context),
-        ),
-      ],
+      ),
     );
   }
 }
