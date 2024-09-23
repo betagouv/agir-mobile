71187a85c7aff53a8d62f070bcf7bfff4b88b46b
Lucas SAUDON <lucas.saudon@beta.gouv.fr>
2024-09-23 14:24:19 +0200

    fix: ajouter image target et mettre à jour DefiWidget

--- app/lib/shared/assets/images.dart
+++ app/lib/shared/assets/images.dart
@@ -9,4 +9,5 @@ abstract final class AssetsImages {
   static const illustration4 = 'assets/images/illustration4.png';
   static const illustration5 = 'assets/images/illustration5.png';
   static const newspaper = 'assets/images/newspaper.png';
+  static const target = 'assets/images/target.png';
 }
