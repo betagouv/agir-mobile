ec2c1325c26d073a2970d8044277d420857bbff9
Lucas SAUDON <lucas.saudon@beta.gouv.fr>
2024-09-26 17:01:20 +0200

    tech: Supprimer code mort suite au remplacement de défi par action

--- app/lib/features/univers/core/domain/defi_id.dart
+++
@@ -1,9 +0,0 @@
-import 'package:equatable/equatable.dart';
-
-class DefiId extends Equatable {
-  const DefiId(this.value);
-  final String value;
-
-  @override
-  List<Object?> get props => [value];
-}
--- app/lib/features/univers/core/domain/univers_port.dart
+++ app/lib/features/univers/core/domain/univers_port.dart
@@ -1,6 +1,4 @@
 import 'package:app/features/univers/core/domain/content_id.dart';
-import 'package:app/features/univers/core/domain/defi.dart';
-import 'package:app/features/univers/core/domain/defi_id.dart';
 import 'package:app/features/univers/core/domain/mission.dart';
 import 'package:app/features/univers/core/domain/mission_liste.dart';
 import 'package:app/features/univers/core/domain/service_item.dart';
@@ -20,26 +18,6 @@ abstract interface class UniversPort {
 
   Future<Either<Exception, Mission>> recupererMission({
     required final String missionId,
-  });
-
-  Future<Either<Exception, Defi>> recupererDefi({
-    required final DefiId defiId,
-  });
-
-  Future<Either<Exception, void>> accepterDefi({
-    required final DefiId defiId,
-  });
-
-  Future<Either<Exception, void>> refuserDefi({
-    required final DefiId defiId,
-    final String? motif,
-  });
-
-  Future<Either<Exception, void>> realiserDefi(final DefiId defiId);
-
-  Future<Either<Exception, void>> abondonnerDefi({
-    required final DefiId defiId,
-    final String? motif,
   });
 
   Future<Either<Exception, void>> gagnerPoints({
