951ec24b5b7c58028ecb4f6872a72a6e46dc307b
Lucas SAUDON <lucas.saudon@beta.gouv.fr>
2024-09-30 17:00:53 +0200

    fix(Bibliotheque): Rendre la carte cliquable

--- app/lib/features/bibliotheque/presentation/pages/contenu.dart
+++ app/lib/features/bibliotheque/presentation/pages/contenu.dart
@@ -11,63 +11,79 @@ class Contenu extends StatelessWidget {
 
   final Recommandation contenu;
 
+  Future<Object?> _goToArticle(final BuildContext context) =>
+      GoRouter.of(context).pushNamed(
+        ArticlePage.name,
+        pathParameters: {'id': contenu.id},
+      );
+
   @override
   Widget build(final BuildContext context) {
     const borderRadius = BorderRadius.all(Radius.circular(DsfrSpacings.s1w));
+
+    const roundedRectangleBorder =
+        RoundedRectangleBorder(borderRadius: borderRadius);
 
     return DecoratedBox(
       decoration: const ShapeDecoration(
         color: Colors.white,
         shadows: recommandationOmbre,
-        shape: RoundedRectangleBorder(borderRadius: borderRadius),
+        shape: roundedRectangleBorder,
       ),
-      child: Column(
-        crossAxisAlignment: CrossAxisAlignment.stretch,
-        children: [
-          ClipRRect(
-            borderRadius: borderRadius.copyWith(
-              bottomLeft: Radius.zero,
-              bottomRight: Radius.zero,
-            ),
-            child: Image.network(contenu.imageUrl, fit: BoxFit.cover),
-          ),
-          Padding(
-            padding: const EdgeInsets.all(DsfrSpacings.s2w),
-            child: Column(
-              crossAxisAlignment: CrossAxisAlignment.stretch,
-              children: [
-                Text(
-                  contenu.thematiqueLabel,
-                  style: const DsfrTextStyle.bodyXsBold(),
+      child: Material(
+        color: Colors.transparent,
+        child: InkWell(
+          onTap: () async => _goToArticle(context),
+          borderRadius: borderRadius,
+          child: Column(
+            crossAxisAlignment: CrossAxisAlignment.stretch,
+            children: [
+              ClipRRect(
+                borderRadius: borderRadius.copyWith(
+                  bottomLeft: Radius.zero,
+                  bottomRight: Radius.zero,
                 ),
-                const SizedBox(height: DsfrSpacings.s1w),
-                Text(
-                  contenu.titre,
-                  style: const DsfrTextStyle.headline4(
-                    color: DsfrColors.blueFranceSun113,
-                  ),
-                ),
-                if (contenu.sousTitre != null) ...[
-                  const SizedBox(height: DsfrSpacings.s2w),
-                  Text(contenu.sousTitre!, style: const DsfrTextStyle.bodyMd()),
-                ],
-                const SizedBox(height: DsfrSpacings.s2w),
-                Align(
-                  alignment: Alignment.centerLeft,
-                  child: DsfrLink.md(
-                    label: Localisation.continuerLaLecture,
-                    icon: DsfrIcons.systemArrowRightLine,
-                    iconPosition: DsfrLinkIconPosition.end,
-                    onTap: () async => GoRouter.of(context).pushNamed(
-                      ArticlePage.name,
-                      pathParameters: {'id': contenu.id},
+                child: Image.network(contenu.imageUrl, fit: BoxFit.cover),
+              ),
+              Padding(
+                padding: const EdgeInsets.all(DsfrSpacings.s2w),
+                child: Column(
+                  crossAxisAlignment: CrossAxisAlignment.stretch,
+                  children: [
+                    Text(
+                      contenu.thematiqueLabel,
+                      style: const DsfrTextStyle.bodyXsBold(),
                     ),
-                  ),
+                    const SizedBox(height: DsfrSpacings.s1w),
+                    Text(
+                      contenu.titre,
+                      style: const DsfrTextStyle.headline4(
+                        color: DsfrColors.blueFranceSun113,
+                      ),
+                    ),
+                    if (contenu.sousTitre != null) ...[
+                      const SizedBox(height: DsfrSpacings.s2w),
+                      Text(
+                        contenu.sousTitre!,
+                        style: const DsfrTextStyle.bodyMd(),
+                      ),
+                    ],
+                    const SizedBox(height: DsfrSpacings.s2w),
+                    Align(
+                      alignment: Alignment.centerLeft,
+                      child: DsfrLink.md(
+                        label: Localisation.continuerLaLecture,
+                        icon: DsfrIcons.systemArrowRightLine,
+                        iconPosition: DsfrLinkIconPosition.end,
+                        onTap: () async => _goToArticle(context),
+                      ),
+                    ),
+                  ],
                 ),
-              ],
-            ),
+              ),
+            ],
           ),
-        ],
+        ),
       ),
     );
   }
--- app/test/old/steps/iel_appuie_sur.dart
+++ app/test/old/steps/iel_appuie_sur.dart
@@ -3,7 +3,7 @@ import 'package:flutter_test/flutter_test.dart';
 
 /// Iel appuie sur.
 Future<void> ielAppuieSur(final WidgetTester tester, final String text) async {
-  await tester.tap(findTextInGestureDetector(text));
+  await tester.tap(findTextInGestureDetector(text).first);
   await tester.pumpAndSettle();
 }
 
