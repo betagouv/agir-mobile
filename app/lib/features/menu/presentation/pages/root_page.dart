bedbacbe106133494bbae391a6b3eaf1746e5954
Lucas SAUDON <lucas.saudon@beta.gouv.fr>
2024-11-06 09:34:38 +0100

    refactor(menu): simplifier la navigation

--- app/lib/features/menu/presentation/pages/root_page.dart
+++ app/lib/features/menu/presentation/pages/root_page.dart
@@ -1,7 +1,6 @@
 import 'package:app/core/presentation/widgets/composants/app_bar.dart';
 import 'package:app/core/presentation/widgets/fondamentaux/colors.dart';
 import 'package:app/features/menu/presentation/widgets/menu.dart';
-import 'package:app/features/menu/presentation/widgets/menu_button.dart';
 import 'package:flutter/material.dart';
 
 class RootPage extends StatelessWidget {
@@ -18,11 +17,7 @@ class RootPage extends StatelessWidget {
 
   @override
   Widget build(final context) => Scaffold(
-        appBar: FnvAppBar(
-          leading: const MenuButton(),
-          title: title,
-          bottom: appBarBottom,
-        ),
+        appBar: FnvAppBar(title: title, bottom: appBarBottom),
         body: body,
         drawer: const Menu(),
         backgroundColor: FnvColors.accueilFond,
