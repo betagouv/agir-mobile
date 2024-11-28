cfb3af6b08038de348eb802c269aa56e83b91d02
Lucas SAUDON <lucas.saudon@beta.gouv.fr>
2024-11-28 15:52:59 +0100

    feat(Endpoints): ajouter des constantes d'API pour les actions utilisateur

---
+++ app/lib/core/infrastructure/endpoints.dart
@@ -0,0 +1,10 @@
+abstract final class Endpoints {
+  Endpoints._();
+
+  /// https://agir-back-dev.osc-fr1.scalingo.io/api#/Defis/DefisController_getAllUserDefi_2
+  static const actions = '/utilisateurs/{userId}/defis_v2';
+
+  /// https://agir-back-dev.osc-fr1.scalingo.io/api#/Defis/DefisController_getById
+  static String action(final String defiId) =>
+      '/utilisateurs/{userId}/defis/{$defiId}';
+}
